32b2ff721d66226280d4eb8cb9d0668e0c17ddf0
Hiroki Noda <kubo39@gmail.com>
2018-02-01 18:46:07 +0900

    python: Add mpmc

--- pychan.py
+++ pychan.py
@@ -8,6 +8,7 @@ THREADS = 4
 seq_queue = queue.Queue()
 spsc_queue = queue.Queue()
 mpsc_queue = queue.Queue()
+mpmc_queue = queue.Queue()
 
 def seq():
     for i in range(0, MESSAGES):
@@ -15,12 +16,12 @@ def seq():
     for i in range(0, MESSAGES):
         seq_queue.get()
 
-def sender():
-    global mpsc_queue
+def mpsc_sender():
+    global mcsc_queue
     for i in range(0, int(MESSAGES / THREADS)):
         mpsc_queue.put(i)
 
-def receiver():
+def mpsc_receiver():
     global mpsc_queue
     for i in range(0, MESSAGES):
         mpsc_queue.get()
@@ -29,12 +30,36 @@ def mpsc():
     global mpsc_queue
     threads = []
     for i in range(0, THREADS):
-        t = threading.Thread(target=sender)
+        t = threading.Thread(target=mpsc_sender)
         t.start()
         threads.append(t)
-    t = threading.Thread(target=receiver)
+    t = threading.Thread(target=mpsc_receiver)
     t.start()
     threads.append(t)
+    for t in threads:
+        t.join()
+
+def mpmc_sender():
+    global mcsc_queue
+    for i in range(0, int(MESSAGES / THREADS)):
+        mpmc_queue.put(i)
+
+def mpmc_receiver():
+    global mpmc_queue
+    for i in range(0, int(MESSAGES / THREADS)):
+        mpmc_queue.get()
+
+def mpmc():
+    global mpmc_queue
+    threads = []
+    for i in range(0, THREADS):
+        t = threading.Thread(target=mpmc_sender)
+        t.start()
+        threads.append(t)
+    for i in range(0, THREADS):
+        t = threading.Thread(target=mpmc_receiver)
+        t.start()
+        threads.append(t)
     for t in threads:
         t.join()
 
@@ -47,3 +72,4 @@ def run(name, f):
 if "__main__" == __name__:
     run("unbounded_seq", seq)
     run("unbounded_mpsc", mpsc)
+    run("unbounded_mpmc", mpmc)
