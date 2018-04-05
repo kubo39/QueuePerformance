aa8dd4aa9dd51e994ddc944628edba36e4d8a6da
2vg <m0fqn090310@gmail.com>
2018-04-05 11:37:53 +0900

    edit: add msq test in "run.sh"

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 rustup run nightly cargo run --release --bin crossbeam-channel | tee channel.txt
 nim c -r -d:release nimchan.nim | tee nim.txt
+nim c -r -d:release --gc: boehm msq.nim | tee nim.txt
 rdmd -O dchan.d | tee dmd.txt
 ruby rbchan.rb | tee ruby.txt
 ./plot.py *.txt
