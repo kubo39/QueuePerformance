9dd8b1dc515d9214719ad81978e478a886213f52
2vg <m0fqn090310@gmail.com>
2018-04-06 22:42:41 +0900

    fix: fix test

--- msq.nim
+++ msq.nim
@@ -78,39 +78,42 @@ proc free*(Q: ptr MSQueue) =
 when isMainModule:
   import os
   import strformat
-  import threadpool
   import times
   
   const MESSAGES = 5_000_000
   const THREADS = 4
   
-  type IntMsQueue = ptr MSQueue[int]
-  var channels: array[0..3, IntMsQueue]
+  type IntChannel = ptr MSQueue[int]
+  var channels: array[0..3, IntChannel]
   
   proc seque =
     channels[0] = msqInitialize[int]()
     for i in 0 .. MESSAGES - 1:
       channels[0].push(int(i))
     for i in 0 .. MESSAGES - 1:
-      var ii: int
-      discard channels[0].pop(ii)
-    sync()
+      var i: int
+      discard channels[0].pop(i)
     channels[0].free()
-  
+
   proc sender1 {.thread.} =
     for i in 0 .. MESSAGES - 1:
       channels[1].push(int(i))
   
   proc receiver1 {.thread.} =
     for i in 0 .. MESSAGES - 1:
-      var ii: int
-      discard channels[1].pop(ii)
+      var i: int
+      discard channels[1].pop(i)
   
   proc spsc =
+    var
+      spscSender: Thread[void]
+      spscReceiver: Thread[void]
+  
     channels[1] = msqInitialize[int]()
-    spawn sender1()
-    spawn receiver1()
-    sync()
+    createThread(spscSender, sender1)
+    createThread(spscReceiver, receiver1)
+    joinThread(spscSender)
+    joinThread(spscReceiver)
     channels[1].free()
   
   proc sender2 {.thread.} =
@@ -119,16 +122,22 @@ when isMainModule:
   
   proc receiver2 {.thread.} =
     for i in 0 .. MESSAGES - 1:
-      var ii: int
-      discard channels[2].pop(ii)
+      var i: int
+      discard channels[2].pop(i)
   
   proc mpsc =
+    var
+      mpscSender: array[0 .. THREADS - 1, Thread[void]]
+      mpscReceiver: Thread[void]
+  
     channels[2] = msqInitialize[int]()
-    for _ in 0 .. THREADS - 1:
-      spawn sender2()
-    spawn receiver2()
-    sync()
+    for i in 0 .. THREADS - 1:
+      createThread(mpscSender[i], sender2)
+    createThread(mpscReceiver, receiver2)
+    joinThreads(mpscSender)
+    joinThread(mpscReceiver)
     channels[2].free()
+  
   
   proc sender3 {.thread.} =
     for i in 0 .. uint(MESSAGES / THREADS) - 1:
@@ -136,26 +145,30 @@ when isMainModule:
   
   proc receiver3 {.thread.} =
     for i in 0 .. uint(MESSAGES / THREADS) - 1:
-      var ii: int
-      discard channels[3].pop(ii)
+      var i: int
+      discard channels[3].pop(i)
   
   proc mpmc =
+    var
+      mpmcSender: array[0 .. THREADS - 1, Thread[void]]
+      mpmcReceiver: array[0 .. THREADS - 1, Thread[void]]
     channels[3] = msqInitialize[int]()
-    for _ in 0 .. THREADS - 1:
-      spawn sender3()
-    for _ in 0 .. THREADS - 1:
-      spawn receiver3()
-    sync()
+    for i in 0 .. THREADS - 1:
+      createThread(mpmcSender[i], sender3)
+    for i in 0 .. THREADS - 1:
+      createThread(mpmcReceiver[i], receiver3)
+    joinThreads(mpmcSender)
+    joinThreads(mpmcReceiver)
     channels[3].free()
   
   proc run(name: string, f: proc()) =
     let time = epochTime()
     f()
     let elapsed = epochTime() - time
-    echo &"""{name:<25} {"msq channel":<15} {elapsed:7.3} sec"""
+    echo &"""{name:<25} {"Nim channel":<15} {elapsed:7.3} sec"""
   
   when isMainModule:
     run("unbounded_seq", seque)
     run("unbounded_spsc", spsc)
     run("unbounded_mpsc", mpsc)
-    run("unbounded_mpmc", mpmc)
+    run("unbounded_mpmc", mpmc)  
